f0cfe1b621605f38ec4b0475b73adaa934183aba
Stephane PERREZ <stephane.perrez@gmail.com>
2021-04-24 15:36:11 +0200

    Added player to GameManager

--- BebePied_project/Assets/00_Content/00_Common/03_GameManager/GameManager.cs
+++ BebePied_project/Assets/00_Content/00_Common/03_GameManager/GameManager.cs
@@ -7,6 +7,7 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     public string FirstSceneToLoad;
+    public Player Player;
     
     private static GameManager instance;
     public void Awake()
