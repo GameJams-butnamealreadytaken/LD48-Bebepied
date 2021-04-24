b1ce2a2368ced7a112a18285df15d2accf28a13b
Kranck <frizzy.rastay@gmail.com>
2021-04-24 15:29:39 +0200

    Added camera in the player. Removed smoothing of FPS camera

--- BebePied_project/Assets/00_Content/00_Common/01_Player/Player.cs
+++ BebePied_project/Assets/00_Content/00_Common/01_Player/Player.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	// Start is called before the first frame update
+	void Start()
+	{
+		//
+		// Lock the cursor 
+		Cursor.lockState = CursorLockMode.Locked;
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		
+	}
+
+	/// <summary>
+	/// KIll the player
+	/// </summary>
+	public void Kill()
+	{
+		Debug.Log("You is dead");
+	}
 }
