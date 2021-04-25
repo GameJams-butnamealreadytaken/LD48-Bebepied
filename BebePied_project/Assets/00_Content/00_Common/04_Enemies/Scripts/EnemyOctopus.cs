605b09b3ba476391d66c6ca786571a20915b6890
Lord-Asterius <loic.mathiot70@gmail.com>
2021-04-25 15:54:24 +0200

    Octopus now descent to the player when near

--- BebePied_project/Assets/00_Content/00_Common/04_Enemies/Scripts/EnemyOctopus.cs
+++ BebePied_project/Assets/00_Content/00_Common/04_Enemies/Scripts/EnemyOctopus.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 public class EnemyOctopus : EnemyBase
 {
     private float VerticalMaxSpeed = 0.02f;
+    private float VerticalMaxSpeedDescentToPlayer = 1f;
     private float CurrentHeightObjective;
+
+    private bool InitialDescent = true;
+    private bool ShouldDescentToPlayer = false;
+    private float DistanceBeforeDescentToPlayer = 15.0f;
 
     protected override void Start()
     {
@@ -24,18 +29,45 @@ public class EnemyOctopus : EnemyBase
     protected override void Update()
     {
         base.Update();
-
-        NavigationAgent.baseOffset -= VerticalMaxSpeed;
-        if (NavigationAgent.baseOffset < -1.0f)
+        
+        if (InitialDescent)
         {
-            // Avoid going too low and colliding with walking enemies
-            NavigationAgent.baseOffset = -1.0f;
+            NavigationAgent.baseOffset -= VerticalMaxSpeed;
+            if (NavigationAgent.baseOffset < -1.0f)
+            {
+                // Avoid going too low and colliding with walking enemies
+                NavigationAgent.baseOffset = -1.0f;
+                InitialDescent = false;
+            }
+
+            return;
+        }
+
+        if (ShouldDescentToPlayer)
+        {
+            NavigationAgent.baseOffset -= VerticalMaxSpeedDescentToPlayer * Time.deltaTime;
+            if (transform.position.y <= Player.transform.position.y)
+            {
+                NavigationAgent.baseOffset += VerticalMaxSpeedDescentToPlayer * Time.deltaTime;    
+            }
+        }
+        else
+        {
+            NavigationAgent.baseOffset += VerticalMaxSpeedDescentToPlayer * Time.deltaTime;
+            if (NavigationAgent.baseOffset >= -1.0f)
+            {
+                NavigationAgent.baseOffset = -1.0f;
+            }
         }
     }
 
     protected override void OnUpdateAI()
     {
         SetDestination(Player.transform.position);
+
+        Vector2 this2DPosition = new Vector2(transform.position.x, transform.position.z);
+        Vector2 player2DPosition = new Vector2(Player.transform.position.x, Player.transform.position.z);
+        ShouldDescentToPlayer = Vector2.Distance(this2DPosition, player2DPosition) <= DistanceBeforeDescentToPlayer;
     }
 
     protected override void OnStartAI()
