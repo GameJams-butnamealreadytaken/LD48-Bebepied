951733cb63c48406dcaf85a9dfbfe2686524db07
Lord-Asterius <loic.mathiot70@gmail.com>
2021-04-26 19:16:30 +0200

    Added chain explosion VFX and SFX

--- BebePied_project/Assets/00_Content/01_Chapters/Scripts/ChainLogic.cs
+++ BebePied_project/Assets/00_Content/01_Chapters/Scripts/ChainLogic.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class ChainLogic : MonoBehaviour
 {
+    public GameObject ExplosionPrefab;
+    public AudioClip ExplosionSound;
+
+    private bool AlreadyCollided = false;
+
     public void OnCollisionEnter(Collision collision)
     {
         transform.parent.GetComponent<LevelLogic>().TriggerEndChainCollision();
@@ -11,6 +16,29 @@ public class ChainLogic : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        if (AlreadyCollided)
+        {
+            return;
+        }
+
+        AlreadyCollided = true;
+        
         transform.parent.GetComponent<LevelLogic>().TriggerEndChainCollision();
+        
+        Invoke(nameof(DestroyEffect), 1.3f);
+    }
+
+    private void DestroyEffect()
+    {
+        GameObject instantiatedObject = Instantiate(ExplosionPrefab, transform.position, transform.rotation, null);
+        ParticleSystem instantiatedParticleSystem = instantiatedObject.GetComponentInChildren<ParticleSystem>();
+        if (instantiatedParticleSystem != null)
+        {
+            instantiatedParticleSystem.Play();
+        }
+
+        AudioSource audioSource = GetComponent<AudioSource>();
+        audioSource.clip = ExplosionSound;
+        audioSource.Play();
     }
 }
